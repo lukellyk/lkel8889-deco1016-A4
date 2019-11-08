634b3a8734868880b65f826c028b1f134838f962
lukellyk <12542270+lukellyk@users.noreply.github.com>
2019-11-08 16:19:42 +1100

    webcam test

--- js/script.js
+++ js/script.js
@@ -17,12 +17,12 @@
       }
     },
 
-    mobileNumber: {
-      presence: true,
-      format:{
-        pattern: "(0?[4])[0-9]{8}",
-      }
-      },
+    // mobileNumber: {
+    //   presence: true,
+    //   format:{
+    //     pattern: "(0?[4])[0-9]{8}",
+    //   }
+    //   },
           
     // }
     // email: {
@@ -198,3 +198,43 @@
     messages.appendChild(block);
   }
 
+// WEBCAM
+
+'use strict';
+
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const snap = document.getElementById("snap");
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+const constraints = {
+  audio: true,
+  video: {
+    width: 1280, height: 720
+  }
+};
+
+// Access webcam
+async function init() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+
+// Success
+function handleSuccess(stream) {
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+// Load init
+init();
+
+// Draw image
+var context = canvas.getContext('2d');
+snap.addEventListener("click", function() {
+	context.drawImage(video, 0, 0, 640, 480);
+});

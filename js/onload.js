31b97d91588cdfa7cc18839057cce673f0f47559
lukellyk <12542270+lukellyk@users.noreply.github.com>
2019-11-09 02:46:33 +1100

    i give up for tonight

--- js/onload.js
+++ js/onload.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', (event) => {
     document.getElementById('thanks-personalised').innerHTML = "Thanks for signing up, "+ localStorage.usersName + "!";
-    document.getElementById('email-personalised').innerHTML = "We've sent you an email to "+ localStorage.usersEmail + " with your login details.";
+    document.getElementById('email-personalised').innerHTML = "Once we've verified your ID we'll send you an email to "+ localStorage.usersEmail + " with your login details.";
   });

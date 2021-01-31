41504afa6854d810aae47ab44de29b3d0b4a368a
lukellyk <luke-k@live.com>
2021-01-31 22:44:49 +1100

    formatting changes lol

--- js/script.js
+++ js/script.js
@@ -1,170 +1,170 @@
 
-  // These are the constraints used to validate the form
-  var form1and2Constraints = {
-    name: {
-      // You need to pick a username too
-      presence: true,
-      // And it must be between 3 and 20 characters long
-      length: {
-        minimum: 3,
-      },
-      format: {
-        // We don't allow anything that a-z and 0-9
-        pattern: "[a-z ]+",
-        // but we don't care if the username is uppercase or lowercase
-        flags: "i",
-        message: "can only contain letters"
-      }
+// These are the constraints used to validate the form
+var form1and2Constraints = {
+  name: {
+    // You need to pick a username too
+    presence: true,
+    // And it must be between 3 and 20 characters long
+    length: {
+      minimum: 3,
     },
-  };
+    format: {
+      // We don't allow anything that a-z and 0-9
+      pattern: "[a-z ]+",
+      // but we don't care if the username is uppercase or lowercase
+      flags: "i",
+      message: "can only contain letters"
+    }
+  },
+};
 
-var form3Constraints = { 
-    email: {
-      // Email is required
-      presence: true,
-      // and must be an email (duh)
-      email: true
-    },  
+var form3Constraints = {
+  email: {
+    // Email is required
+    presence: true,
+    // and must be an email (duh)
+    email: true
+  },
 }
 
-var form4Constraints = { 
+var form4Constraints = {
 }
 
-var form5Constraints = { 
+var form5Constraints = {
 }
 
 
-  // Hook up the form so we can prevent it from being posted
-  var form = document.querySelector("#form1");
-  form.addEventListener("submit", function(ev) {
-    ev.preventDefault();
-    handleFormSubmit(form, undefined, form1and2Constraints);
-    localStorage.setItem("usersName", inputs.item(1).value.charAt(0).toUpperCase() + inputs.item(1).value.slice(1))
-    document.getElementById('gday-text').innerHTML = "G'day, "+ localStorage.usersName + "!";
-  });
+// Hook up the form so we can prevent it from being posted
+var form = document.querySelector("#form1");
+form.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  handleFormSubmit(form, undefined, form1and2Constraints);
+  localStorage.setItem("usersName", inputs.item(1).value.charAt(0).toUpperCase() + inputs.item(1).value.slice(1))
+  document.getElementById('gday-text').innerHTML = "G'day, " + localStorage.usersName + "!";
+});
 
 
-  var form2 = document.querySelector("#form2");
-  form2.addEventListener("submit", function(ev) {
-    ev.preventDefault();
-    handleFormSubmit(form2, undefined, form1and2Constraints);
-  });
+var form2 = document.querySelector("#form2");
+form2.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  handleFormSubmit(form2, undefined, form1and2Constraints);
+});
 
-  var form3 = document.querySelector("#form3");
-  form3.addEventListener("submit", function(ev) {
-    ev.preventDefault();
-    handleFormSubmit(form3, undefined, form3Constraints);
-    localStorage.setItem("usersEmail", inputs.item(6).value)
-  });
+var form3 = document.querySelector("#form3");
+form3.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  handleFormSubmit(form3, undefined, form3Constraints);
+  localStorage.setItem("usersEmail", inputs.item(6).value)
+});
 
-  var form4 = document.querySelector("#form4");
-  form4.addEventListener("submit", function(ev) {
-    ev.preventDefault();
-    handleFormSubmit(form4, undefined, form4Constraints);
-  });
+var form4 = document.querySelector("#form4");
+form4.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  handleFormSubmit(form4, undefined, form4Constraints);
+});
 
-  var form5 = document.querySelector("#form5");
-  form5.addEventListener("submit", function(ev) {
-    ev.preventDefault();
-    handleFormSubmit(form5, undefined, form5Constraints);
-  });
+var form5 = document.querySelector("#form5");
+form5.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  handleFormSubmit(form5, undefined, form5Constraints);
+});
 
 
 
-  function handleFormSubmit(form, input, constraints) {
-    // validate the form against the constraints
-    var errors = validate(form, constraints);
-    // then we update the form to reflect the results
-    showErrors(form, errors || {});
-    if (!errors) {
-      // showSuccess();
-      fullpage_api.moveSectionDown();
-    }
+function handleFormSubmit(form, input, constraints) {
+  // validate the form against the constraints
+  var errors = validate(form, constraints);
+  // then we update the form to reflect the results
+  showErrors(form, errors || {});
+  if (!errors) {
+    // showSuccess();
+    fullpage_api.moveSectionDown();
   }
+}
 
-  function showSuccess() {
-    // We made it \:D/
-    alert("Success!");
-  }
-
-
-
+function showSuccess() {
+  // We made it \:D/
+  alert("Success!");
+}
 
 
 
-  // Hook up the inputs to validate on the fly
-  var inputs = document.querySelectorAll("input, textarea, select");
-  console.log(inputs);
-  for (var i = 0; i < inputs.length; ++i) {
-    inputs.item(i).addEventListener("change", function(ev) {
 
-      var errors = validate(form, {...form1and2Constraints, ...form3Constraints}) || {};
-      showErrorsForInput(this, errors[this.name])
+
+
+// Hook up the inputs to validate on the fly
+var inputs = document.querySelectorAll("input, textarea, select");
+console.log(inputs);
+for (var i = 0; i < inputs.length; ++i) {
+  inputs.item(i).addEventListener("change", function (ev) {
+
+    var errors = validate(form, { ...form1and2Constraints, ...form3Constraints }) || {};
+    showErrorsForInput(this, errors[this.name])
+  });
+}
+
+
+// Updates the inputs with the validation errors
+function showErrors(form, errors) {
+  // We loop through all the inputs and show the errors for that input
+  form.querySelectorAll("input[name], select[name]").forEach(function (input) {
+    // Since the errors can be null if no errors were found we need to handle
+    // that
+    showErrorsForInput(input, errors && errors[input.name]);
+  });
+}
+
+// Shows the errors for a specific input
+function showErrorsForInput(input, errors) {
+  // This is the root of the input
+  var formGroup = closestParent(input.parentNode, "form-group")
+    // Find where the error messages will be insert into
+    , messages = formGroup.querySelector(".messages");
+  // First we remove any old messages and resets the classes
+  resetFormGroup(formGroup);
+  // If we have errors
+  if (errors) {
+    // we first mark the group has having errors
+    formGroup.classList.add("has-error");
+    // then we append all the errors
+    errors.forEach(function (error) {
+      addError(messages, error);
     });
+  } else {
+    // otherwise we simply mark it as success
+    formGroup.classList.add("has-success");
   }
+}
 
-
-  // Updates the inputs with the validation errors
-  function showErrors(form, errors) {
-    // We loop through all the inputs and show the errors for that input
-    form.querySelectorAll("input[name], select[name]").forEach( function(input) {
-      // Since the errors can be null if no errors were found we need to handle
-      // that
-      showErrorsForInput(input, errors && errors[input.name]);
-    });
+// Recusively finds the closest parent that has the specified class
+function closestParent(child, className) {
+  if (!child || child == document) {
+    return null;
   }
-
-  // Shows the errors for a specific input
-  function showErrorsForInput(input, errors) {
-    // This is the root of the input
-    var formGroup = closestParent(input.parentNode, "form-group")
-      // Find where the error messages will be insert into
-      , messages = formGroup.querySelector(".messages");
-    // First we remove any old messages and resets the classes
-    resetFormGroup(formGroup);
-    // If we have errors
-    if (errors) {
-      // we first mark the group has having errors
-      formGroup.classList.add("has-error");
-      // then we append all the errors
-      errors.forEach(function(error) {
-        addError(messages, error);
-      });
-    } else {
-      // otherwise we simply mark it as success
-      formGroup.classList.add("has-success");
-    }
+  if (child.classList.contains(className)) {
+    return child;
+  } else {
+    return closestParent(child.parentNode, className);
   }
+}
 
-  // Recusively finds the closest parent that has the specified class
-  function closestParent(child, className) {
-    if (!child || child == document) {
-      return null;
-    }
-    if (child.classList.contains(className)) {
-      return child;
-    } else {
-      return closestParent(child.parentNode, className);
-    }
-  }
+function resetFormGroup(formGroup) {
+  // Remove the success and error classes
+  formGroup.classList.remove("has-error");
+  formGroup.classList.remove("has-success");
+  // and remove any old messages
+  formGroup.querySelectorAll(".help-block.error").forEach(function (el) {
+    el.parentNode.removeChild(el);
+  });
+}
 
-  function resetFormGroup(formGroup) {
-    // Remove the success and error classes
-    formGroup.classList.remove("has-error");
-    formGroup.classList.remove("has-success");
-    // and remove any old messages
-    formGroup.querySelectorAll(".help-block.error").forEach(function(el) {
-      el.parentNode.removeChild(el);
-    });
-  }
-
-  // Adds the specified error with the following markup
-  // <p class="help-block error">[message]</p>
-  function addError(messages, error) {
-    var block = document.createElement("p");
-    block.classList.add("help-block");
-    block.classList.add("error");
-    block.innerText = error;
-    messages.appendChild(block);
-  }
+// Adds the specified error with the following markup
+// <p class="help-block error">[message]</p>
+function addError(messages, error) {
+  var block = document.createElement("p");
+  block.classList.add("help-block");
+  block.classList.add("error");
+  block.innerText = error;
+  messages.appendChild(block);
+}
 
